e9e79d36334fb8d00c5e110f105f45619e0bd9f5
TheAziraphale <danielh_7@hotmail.com>
2022-07-02 06:58:07 +0200

    Updated healthSum to be dmgSum and made code cleaner

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -26,9 +26,9 @@ export class VampireActor extends Actor {
       actorData.health.max = 7
     }
 
-    const healthSum = actorData.health.superficial + actorData.health.aggravated + actorData.health.lethal - (actorData.health.max - 7)
-    console.log(healthSum, WoundLevels[healthSum])
-    actorData.health.state = healthSum > 0 ? WoundLevels[0] : WoundLevels[healthSum] ? WoundLevels[healthSum] : 'Outside Bounds'
+    const dmgSum = Math.max(actorData.health.superficial + actorData.health.aggravated + actorData.health.lethal - (actorData.health.max - 7), 0)
+    console.log(dmgSum, WoundLevels[dmgSum])
+    actorData.health.state = WoundLevels[dmgSum] ? WoundLevels[dmgSum] : 'Outside Bounds'
   }
 
   /**
