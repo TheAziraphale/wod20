f3f89fb776d38e72e8dedd7a4cf67728f783b4b0
TheAziraphale <danielh_7@hotmail.com>
2022-07-03 03:55:02 +0200

    losing battle against the wrong labels on attributes/abilities

--- module/actor/mortal-actor-sheet.js
+++ module/actor/mortal-actor-sheet.js
@@ -162,7 +162,7 @@ export class MortalActorSheet extends CoterieActorSheet {
       wounded = ``
       applyWounds = ``
     } else {
-      if (dataset.rollingattributes=="true") {
+      if (dataset.ability=="true") {
         selectAbility =  `<div class="form-group">
                             <label>${game.i18n.localize("VTM5E.SelectAbility")}</label>
                             <select id="abilitySelect">${options}</select>
