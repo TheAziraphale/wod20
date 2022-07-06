d67b4ede9257d1df692632f5f62dd2a3c66f864a
TheAziraphale <danielh_7@hotmail.com>
2022-07-06 02:39:47 +0200

    Fixed negative true/false check

--- module/actor/roll-dice.js
+++ module/actor/roll-dice.js
@@ -145,17 +145,8 @@ export async function rollInit (
   
   console.log(token, foundToken, foundEncounter, rolledInitiative, tokenAdded)
   if (foundToken && foundEncounter) {
-    if (_inTurn(token)) {
-      console.log(token.combatant)
-      console.log(token)
+    if (!_inTurn(token)) {
       await token.toggleCombat();
-
-      console.log(token.combatant)
-
-      if(token.combatant) {
-        console.log(token.combatant.data.initiativ)
-      }
-
       if (token.combatant && token.combatant.data.initiative == undefined) {
         console.log("Updating token init to", finalValue)
         console.log(finalValue)
