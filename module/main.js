68bc944888b27a6c9ba9184105945c2dff43b4f6
TheAziraphale <danielh_7@hotmail.com>
2022-07-10 03:14:32 +0200

    Had to add a specific else if eq as its problem with nested

--- module/main.js
+++ module/main.js
@@ -331,6 +331,16 @@ Hooks.once("init", async function () {
     return 10 - humanity - stain;
   });
 
+  Handlebars.registerHelper('if_eq', function () {
+    const args = Array.prototype.slice.call(arguments, 0, -1);
+      const options = arguments[arguments.length - 1];
+    const allEqual = args.every(function (expression) {
+        return args[0] === expression;
+      });
+      
+      return allEqual ? options.fn(this) : options.inverse(this);
+  });
+  
   Handlebars.registerHelper("numLoop", function (num, options) {
     let ret = "";
 
