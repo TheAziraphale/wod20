fdc47c631f9c885355f936897938bd3a66817e06
TheAziraphale <danielh_7@hotmail.com>
2022-07-10 23:01:20 +0200

    Adding some logs

--- module/actor/ghoul-actor-sheet.js
+++ module/actor/ghoul-actor-sheet.js
@@ -134,7 +134,9 @@ export class GhoulActorSheet extends MortalActorSheet {
 
       console.log(this.actor.data)
       if(this.actor.data.data.disciplines[data.discipline]?.isCustom) {
+        console.log(this.actor.data.data.disciplines[data.discipline])
         delete this.actor.data.data.disciplines[data.discipline]
+        console.log(this.actor.data.data.disciplines)
         this.actor.update({
           [`data.disciplines`]: this.actor.data.data.disciplines,
         });
