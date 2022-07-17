16289ae028582c74ffd0ec5942f006d2620c7553
TheAziraphale <danielh_7@hotmail.com>
2022-07-17 22:11:14 +0200

    Try again in the log

--- module/actor/ghoul-actor-sheet.js
+++ module/actor/ghoul-actor-sheet.js
@@ -105,7 +105,7 @@ export class GhoulActorSheet extends MortalActorSheet {
         if (i.data.discipline !== undefined) {
           console.log("that's the discipline", i.data.discipline);
           disciplines[i.data.discipline] && disciplines[i.data.discipline].push(i);
-          console.log(this.actor.data.data.disciplines[i.data.discipline], !this.actor.data.data.disciplines[i.data.discipline].visible)
+          console.log(i.data.discipline, this.actor.data.data.disciplines[i.data.discipline], this.actor.data.data.disciplines[i.data.discipline] && !this.actor.data.data.disciplines[i.data.discipline].visible)
           if (this.actor.data.data.disciplines[i.data.discipline] && !this.actor.data.data.disciplines[i.data.discipline].visible) {
             console.log("set it to visible", i.data.discipline)
             this.actor.update({
